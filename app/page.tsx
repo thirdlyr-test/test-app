c66a7c45cab9ae3d93cf595c322d327309fa2fa0
Jackie <jackietanyen@gmail.com>
2025-01-21 14:03:08 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Sidebar from "@/components/Sidebar";
+import MainNavbar from "@/components/MainNavbar";
 
 export default function Page() {
   return (
@@ -8,34 +9,9 @@ export default function Page() {
 
       {/* Main Content Column */}
       <div className="flex-1 bg-gray-50 overflow-y-auto">
-        {/* Main Content */}
         <div className="max-w-[1016px] mx-auto px-[64px]">
           {/* Navbar */}
-          <div className="h-[136px] flex items-center justify-end bg-white border-b border-gray-200">
-            <div className="flex items-center space-x-4">
-              {/* Profile Section */}
-              <div className="w-10 h-10 bg-gray-300 text-white flex items-center justify-center rounded-full">
-                JT
-              </div>
-              <span className="text-gray-700 font-medium">Jackie Tan</span>
-              <span
-                className="ml-1 hidden sm:inline transform rotate-90 text-gray-500"
-                data-testid="chevron-right-icon"
-              >
-                <svg
-                  aria-hidden="true"
-                  focusable="false"
-                  role="none"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M12.771 7.115a.83.83 0 0 0-1.2 0L3 15.686l1.2 1.2 7.971-7.971 7.972 7.971 1.2-1.2z"></path>
-                </svg>
-              </span>
-            </div>
-          </div>
+          <MainNavbar />
 
           {/* Total Balance Section */}
           <section className="mt-8 bg-white p-6 rounded-lg shadow border border-gray-200">
@@ -44,7 +20,9 @@ export default function Page() {
                 <h1 className="text-base font-semibold text-gray-600">
                   Total balance
                 </h1>
-                <p className="mt-2 text-3xl font-bold text-gray-900">1.13 SGD</p>
+                <p className="mt-2 text-3xl font-bold text-gray-900">
+                  1.13 SGD
+                </p>
               </div>
               {/* Action Buttons */}
               <div className="flex space-x-2">
